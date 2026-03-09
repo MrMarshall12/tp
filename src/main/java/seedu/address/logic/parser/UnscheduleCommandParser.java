bc23b280898a201fddaeb1da4943d50cd7c2c37f
DenseLance-alt <e1406571@u.nus.edu>
2026-03-09 13:23:48 +0800

    Add parser for unschedule

--- src/main/java/seedu/address/logic/parser/UnscheduleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnscheduleCommandParser.java
@@ -3,24 +3,23 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.UnscheduleCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new UnscheduleCommand object
  */
-public class DeleteCommandParser implements Parser<DeleteCommand> {
+public class UnscheduleCommandParser implements Parser<UnscheduleCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns a DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the UnscheduleCommand
+     * and returns a UnscheduleCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteCommand parse(String args) throws ParseException {
+    public UnscheduleCommand parse(String args) throws ParseException {
         try {
             Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            return new UnscheduleCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnscheduleCommand.MESSAGE_USAGE), pe);
