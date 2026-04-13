edf1f57365d97580687f80a94ec20f8ee56886ee
BenedTj <benedicttjia.prof@gmail.com>
2026-04-14 01:24:17 +0800

    Remove test case repetition

--- src/test/java/seedu/address/model/delivery/EndDateTest.java
+++ src/test/java/seedu/address/model/delivery/EndDateTest.java
@@ -30,28 +30,12 @@ public class EndDateTest {
         // Boundary value: empty string
         assertFalse(EndDate.isValidEndDate(""));
         assertFalse(EndDate.isValidEndDate(" ")); // spaces only
-        assertFalse(EndDate.isValidEndDate("12")); // only date number
-        assertFalse(EndDate.isValidEndDate("2012")); // only year
-        assertFalse(EndDate.isValidEndDate("01-12")); // does not contain year
-        assertFalse(EndDate.isValidEndDate("2020-01")); // does not contain date number
 
         // EP: string with date of the wrong number of digits -> returns false
         assertFalse(EndDate.isValidEndDate("2020-1-01")); // month with only 1 digit
-        assertFalse(EndDate.isValidEndDate("2020-123-01")); // month with more than 2 digits
-        assertFalse(EndDate.isValidEndDate("2020-11-2")); // day with only 1 digit
-        assertFalse(EndDate.isValidEndDate("2020-11-401")); // day with more than 2 digits
-        // years with more than 4 digits
-        assertFalse(EndDate.isValidEndDate("+99999999-12-31"));
-        assertFalse(EndDate.isValidEndDate("-99999999-01-01"));
 
         // EP: string with date of the correct number of digits but incorrect format and invalidity -> returns false
         assertFalse(EndDate.isValidEndDate("12-01-2026")); // incorrect format
-        assertFalse(EndDate.isValidEndDate("2020-02-31")); // invalid date
-        assertFalse(EndDate.isValidEndDate("2021-02-29")); // invalid date for non-leap year
-        assertFalse(EndDate.isValidEndDate("-1000-12-3")); // negative year
-        assertFalse(EndDate.isValidEndDate("2020-15-01")); // invalid month
-        assertFalse(EndDate.isValidEndDate("2020-11-31")); // invalid day for month
-        assertFalse(EndDate.isValidEndDate("2020-11-63")); // invalid day
 
         // EP: string with valid date of the correct number of digits -> returns true
         assertTrue(EndDate.isValidEndDate("2019-10-15")); // correct format
